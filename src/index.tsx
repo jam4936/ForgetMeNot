37894e5ad403e704e2a82006af4d60650d8719cd
kgo6331 <43654121+kgo6331@users.noreply.github.com>
2022-09-27 15:15:11 -0400

    Added the /projects page
    Copied the projects page so that will be our new website

--- src/index.tsx
+++ src/index.tsx
@@ -13,9 +13,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-      <AmplifyProvider>
-          <App/>
-      </AmplifyProvider>
-  </React.StrictMode>
+    <AmplifyProvider>
+        <App/>
+    </AmplifyProvider>
 );
