de55dc389f72bafdafb83807274cf340f91c6f95
Jarred Moyer <jarredds941@gmail.com>
2022-10-03 10:06:07 -0400

    Removed code, added output element

--- src/Vision/Vision.tsx
+++ src/Vision/Vision.tsx
@@ -4,12 +4,13 @@ import * as faceapi from 'face-api.js'
 class Vision extends React.Component {
     videoElement: any;
     canvasElement: any;
-    cvModel: any
- 
+    cvModel: any;
+    outputElement: any;
     constructor(props: {} | Readonly<{}>){
         super(props)
         this.videoElement = React.createRef();
         this.canvasElement = React.createRef();
+        this.outputElement = React.createRef();
     }
     getFaceDetectorOptions() {
         const minConfidence = 0.5
@@ -39,9 +40,11 @@ class Vision extends React.Component {
         const result = await faceapi.detectSingleFace(videoEl, options)
         
         if (result) {
+            console.log(result)
             const canvas = this.canvasElement.current;
             const dims = faceapi.matchDimensions(canvas, videoEl, true)
             faceapi.draw.drawDetections(canvas, faceapi.resizeResults(result, dims))
+            this.outputElement.current.value = result.score>.6
             }
     
             setTimeout(() => this.onPlay())
@@ -80,65 +83,16 @@ class Vision extends React.Component {
                     </div>
 
                     <div className="row side-by-side">
-
-
-                        <div id="face_detector_selection_control" className="row input-field">
-                            <select id="selectFaceDetector">
-                                <option value="ssd_mobilenetv1">SSD Mobilenet V1</option>
-                                <option value="tiny_face_detector">Tiny Face Detector</option>
-                            </select>
-                            <label>Select Face Detector</label>
-                        </div>
-
-
-
                         <div id="fps_meter" className="row side-by-side">
                             <div>
                                 <label>Time:</label>
                                 <input disabled value="-" id="time" type="text" className="bold"/>
                                     <label>Is Face There?: </label>
-                                    <input disabled value="-" id="fps" type="text" className="bold"/>
+                                    <input ref={this.outputElement} disabled value="-" id="fps" type="text" className="bold"/>
                                     </div>
                             </div>
 
-
                         </div>
-
-
-
-                        <span id="ssd_mobilenetv1_controls">
-                            <div className="row side-by-side">
-                                <div className="row">
-                                    <label>Min Confidence:</label>
-                                    <input disabled value="0.5" id="minConfidence" type="text" className="bold"/>
-                                </div>
-                            
-                        
-                            </div>
-                        </span>
-
-                        <span id="tiny_face_detector_controls">
-                            <div className="row side-by-side">
-                                <div className="row input-field">
-                                    <select id="inputSize">
-                                        <option value="" disabled selected>Input Size:</option>
-                                        <option value="128">128 x 128</option>
-                                        <option value="160">160 x 160</option>
-                                        <option value="224">224 x 224</option>
-                                        <option value="320">320 x 320</option>
-                                        <option value="416">416 x 416</option>
-                                        <option value="512">512 x 512</option>
-                                        <option value="608">608 x 608</option>
-                                    </select>
-                                    <label>Input Size</label>
-                                </div>
-                                <div className="row">
-                                    <label>Score Threshold:</label>
-                                    <input disabled value="0.5" id="scoreThreshold" type="text" className="bold"/>
-                                </div>
-                                
-                            </div>
-                        </span>
                     </div>
                     
             </body>);
