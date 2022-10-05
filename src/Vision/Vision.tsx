2cbdab8118555ebdbeb5b4c520885c28d33cc583
Jarred Moyer <jarredds941@gmail.com>
2022-10-05 11:35:40 -0400

    Vision, added a detection algo

--- src/Vision/Vision.tsx
+++ src/Vision/Vision.tsx
@@ -6,6 +6,8 @@ class Vision extends React.Component {
     canvasElement: any;
     cvModel: any;
     outputElement: any;
+    distancePairings = [[1,28,17],[1,29,17],[2,30,16],[2,31,16]]
+
     constructor(props: {} | Readonly<{}>){
         super(props)
         this.videoElement = React.createRef();
@@ -21,7 +23,15 @@ class Vision extends React.Component {
         return new faceapi.SsdMobilenetv1Options({ minConfidence })
     }
 
-    
+    getNose(landmarks:any){
+        return [landmarks[28],landmarks[29],landmarks[30],landmarks[31]]
+    }
+    getRight(landmarks:any){
+        return [landmarks[17],landmarks[16]]
+    }
+    getLeft(landmarks:any){
+        return [landmarks[1],landmarks[2]]
+    }
     async onPlay(this:any){
         
         const videoEl = this.videoElement.current;
@@ -40,23 +50,65 @@ class Vision extends React.Component {
         const result = await faceapi.detectSingleFace(videoEl, options).withFaceLandmarks()
         
         if (result) {
-            console.log(result)
+            //console.log(result)
             const canvas = this.canvasElement.current;
             const dims = faceapi.matchDimensions(canvas, videoEl, true)
             const resizedResult = faceapi.resizeResults(result, dims)
-            const landmarks = result.landmarks.positions;
-            const pointA = landmarks[9]
-            const pointB = landmarks[28]
-            const pointC = landmarks[31]
-            const pointD = landmarks[30]
-            const pointE = landmarks[29]
-            //28-31
+            const landmarksFromResults = result.landmarks.positions;
+            let xMax = 0
+            let yMax = 0
+            let normalizedPoints: any[] = []
+            landmarksFromResults.forEach(e=>{
+                
+                if(e.x>xMax){
+                    xMax = e.x
+                }
+                if(e.y>yMax){
+                    yMax  = e.y
+                }
+            })
+            let count = 0
+            landmarksFromResults.forEach(e=>{
+                let normal = {x : 0, y: 0, count:count}
+                normal.x  = e.x/xMax
+                normal.y = e.y/yMax
+                count++;
+                normalizedPoints.push(normal)
+                
+            })
+            //console.log(normalizedPoints)
+            let runningSum = 0;
+            this.distancePairings.forEach((element: any[]) => {
+                //console.log(element)
+                let leftP = normalizedPoints[element[0]]
+                let midP = normalizedPoints[element[1]]
+                let rightP = normalizedPoints[element[2]]
+                const leftDist =  Math.sqrt(((leftP.x-midP.x)*(leftP.x-midP.x))+((leftP.y-midP.y)*((leftP.y-midP.y))))
+                const rightDist =  Math.sqrt(((rightP.x-midP.x)*(rightP.x-midP.x))+((rightP.y-midP.y)*((rightP.y-midP.y))))  
+                if(0==runningSum){
+                    runningSum =Math.abs(leftDist-rightDist)
+                }
+                else{
+                    runningSum =  runningSum + Math.abs(leftDist-rightDist)
+                }
+             
+
+            });
+            console.log(runningSum)
+            if(.5>runningSum && runningSum>.1){
+                this.outputElement.current.style.backgroundColor="#00B1E1"
+            }
+            else{
+                
+                this.outputElement.current.style.backgroundColor="#E9573F"
+            }
+            
 
             if (true) {
             faceapi.draw.drawDetections(canvas, resizedResult)
             }
             faceapi.draw.drawFaceLandmarks(canvas, resizedResult)
-            //this.outputElement.current.value = result.score>.6
+            
             }
     
             setTimeout(() => this.onPlay())
