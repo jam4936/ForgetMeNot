8cea61b195efc83b425ad519b91fa4dcaf5663a2
kgo6331 <43654121+kgo6331@users.noreply.github.com>
2022-10-06 11:08:10 -0400

    Testing to see whether the amplify backend was needed

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -5,5 +5,22 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "auth": {
+        "Forgetmenot": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "s3forgetmenotstorageac93c2ac": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
--- src/models/schema.js
+++ src/models/schema.js
@@ -1,7 +1,173 @@
 export const schema = {
     "models": {
-        "Patient": {
-            "name": "Patient",
+        "Question": {
+            "name": "Question",
+            "fields": {
+                "id": {
+                    "name": "id",
+                    "isArray": false,
+                    "type": "ID",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "prompt": {
+                    "name": "prompt",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": false,
+                    "attributes": []
+                },
+                "response": {
+                    "name": "response",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": false,
+                    "attributes": []
+                },
+                "patientID": {
+                    "name": "patientID",
+                    "isArray": false,
+                    "type": "ID",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "createdAt": {
+                    "name": "createdAt",
+                    "isArray": false,
+                    "type": "AWSDateTime",
+                    "isRequired": false,
+                    "attributes": [],
+                    "isReadOnly": true
+                },
+                "updatedAt": {
+                    "name": "updatedAt",
+                    "isArray": false,
+                    "type": "AWSDateTime",
+                    "isRequired": false,
+                    "attributes": [],
+                    "isReadOnly": true
+                }
+            },
+            "syncable": true,
+            "pluralName": "Questions",
+            "attributes": [
+                {
+                    "type": "model",
+                    "properties": {}
+                },
+                {
+                    "type": "key",
+                    "properties": {
+                        "name": "byPatient",
+                        "fields": [
+                            "patientID"
+                        ]
+                    }
+                },
+                {
+                    "type": "auth",
+                    "properties": {
+                        "rules": [
+                            {
+                                "allow": "private",
+                                "operations": [
+                                    "create",
+                                    "update",
+                                    "delete",
+                                    "read"
+                                ]
+                            }
+                        ]
+                    }
+                }
+            ]
+        },
+        "Routine": {
+            "name": "Routine",
+            "fields": {
+                "id": {
+                    "name": "id",
+                    "isArray": false,
+                    "type": "ID",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "timeOfDay": {
+                    "name": "timeOfDay",
+                    "isArray": false,
+                    "type": {
+                        "enum": "TimeOfDay"
+                    },
+                    "isRequired": false,
+                    "attributes": []
+                },
+                "routineExplanation": {
+                    "name": "routineExplanation",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": false,
+                    "attributes": []
+                },
+                "patientID": {
+                    "name": "patientID",
+                    "isArray": false,
+                    "type": "ID",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "createdAt": {
+                    "name": "createdAt",
+                    "isArray": false,
+                    "type": "AWSDateTime",
+                    "isRequired": false,
+                    "attributes": [],
+                    "isReadOnly": true
+                },
+                "updatedAt": {
+                    "name": "updatedAt",
+                    "isArray": false,
+                    "type": "AWSDateTime",
+                    "isRequired": false,
+                    "attributes": [],
+                    "isReadOnly": true
+                }
+            },
+            "syncable": true,
+            "pluralName": "Routines",
+            "attributes": [
+                {
+                    "type": "model",
+                    "properties": {}
+                },
+                {
+                    "type": "key",
+                    "properties": {
+                        "name": "byPatient",
+                        "fields": [
+                            "patientID"
+                        ]
+                    }
+                },
+                {
+                    "type": "auth",
+                    "properties": {
+                        "rules": [
+                            {
+                                "allow": "private",
+                                "operations": [
+                                    "create",
+                                    "update",
+                                    "delete",
+                                    "read"
+                                ]
+                            }
+                        ]
+                    }
+                }
+            ]
+        },
+        "Relative": {
+            "name": "Relative",
             "fields": {
                 "id": {
                     "name": "id",
@@ -23,6 +189,197 @@ export const schema = {
                     "type": "String",
                     "isRequired": false,
                     "attributes": []
+                },
+                "relationship": {
+                    "name": "relationship",
+                    "isArray": false,
+                    "type": {
+                        "enum": "Relationship"
+                    },
+                    "isRequired": false,
+                    "attributes": []
+                },
+                "patientID": {
+                    "name": "patientID",
+                    "isArray": false,
+                    "type": "ID",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "createdAt": {
+                    "name": "createdAt",
+                    "isArray": false,
+                    "type": "AWSDateTime",
+                    "isRequired": false,
+                    "attributes": [],
+                    "isReadOnly": true
+                },
+                "updatedAt": {
+                    "name": "updatedAt",
+                    "isArray": false,
+                    "type": "AWSDateTime",
+                    "isRequired": false,
+                    "attributes": [],
+                    "isReadOnly": true
+                }
+            },
+            "syncable": true,
+            "pluralName": "Relatives",
+            "attributes": [
+                {
+                    "type": "model",
+                    "properties": {}
+                },
+                {
+                    "type": "key",
+                    "properties": {
+                        "name": "byPatient",
+                        "fields": [
+                            "patientID"
+                        ]
+                    }
+                },
+                {
+                    "type": "auth",
+                    "properties": {
+                        "rules": [
+                            {
+                                "allow": "private",
+                                "operations": [
+                                    "create",
+                                    "update",
+                                    "delete",
+                                    "read"
+                                ]
+                            }
+                        ]
+                    }
+                }
+            ]
+        },
+        "Patient": {
+            "name": "Patient",
+            "fields": {
+                "id": {
+                    "name": "id",
+                    "isArray": false,
+                    "type": "ID",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "location": {
+                    "name": "location",
+                    "isArray": false,
+                    "type": {
+                        "enum": "Locations"
+                    },
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "firstName": {
+                    "name": "firstName",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "lastName": {
+                    "name": "lastName",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "birthPlace": {
+                    "name": "birthPlace",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": false,
+                    "attributes": []
+                },
+                "dateOfBirth": {
+                    "name": "dateOfBirth",
+                    "isArray": false,
+                    "type": "AWSDate",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "Relatives": {
+                    "name": "Relatives",
+                    "isArray": true,
+                    "type": {
+                        "model": "Relative"
+                    },
+                    "isRequired": false,
+                    "attributes": [],
+                    "isArrayNullable": true,
+                    "association": {
+                        "connectionType": "HAS_MANY",
+                        "associatedWith": "patientID"
+                    }
+                },
+                "education": {
+                    "name": "education",
+                    "isArray": true,
+                    "type": {
+                        "enum": "Education"
+                    },
+                    "isRequired": false,
+                    "attributes": [],
+                    "isArrayNullable": true
+                },
+                "personalityTraits": {
+                    "name": "personalityTraits",
+                    "isArray": true,
+                    "type": {
+                        "enum": "PersonalityTrait"
+                    },
+                    "isRequired": false,
+                    "attributes": [],
+                    "isArrayNullable": true
+                },
+                "notes": {
+                    "name": "notes",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": false,
+                    "attributes": []
+                },
+                "Routines": {
+                    "name": "Routines",
+                    "isArray": true,
+                    "type": {
+                        "model": "Routine"
+                    },
+                    "isRequired": false,
+                    "attributes": [],
+                    "isArrayNullable": true,
+                    "association": {
+                        "connectionType": "HAS_MANY",
+                        "associatedWith": "patientID"
+                    }
+                },
+                "familyEmails": {
+                    "name": "familyEmails",
+                    "isArray": true,
+                    "type": "AWSEmail",
+                    "isRequired": false,
+                    "attributes": [],
+                    "isArrayNullable": true
+                },
+                "Questions": {
+                    "name": "Questions",
+                    "isArray": true,
+                    "type": {
+                        "model": "Question"
+                    },
+                    "isRequired": false,
+                    "attributes": [],
+                    "isArrayNullable": true,
+                    "association": {
+                        "connectionType": "HAS_MANY",
+                        "associatedWith": "patientID"
+                    }
                 },
                 "createdAt": {
                     "name": "createdAt",
@@ -67,7 +424,110 @@ export const schema = {
             ]
         }
     },
-    "enums": {},
+    "enums": {
+        "TimeOfDay": {
+            "name": "TimeOfDay",
+            "values": [
+                "MORNING",
+                "AFTERNOON",
+                "EVENING",
+                "BEDTIME"
+            ]
+        },
+        "PersonalityTrait": {
+            "name": "PersonalityTrait",
+            "values": [
+                "EXT1",
+                "EXT2",
+                "FRI1",
+                "FRI2",
+                "HAP1",
+                "HAP2",
+                "INT1",
+                "INT2",
+                "RES1",
+                "RES2",
+                "SER1",
+                "SER2",
+                "SUS1",
+                "SUS2",
+                "TIM1",
+                "TIM2",
+                "CAR1",
+                "CAR2",
+                "CLE1",
+                "CLE2",
+                "CLV1",
+                "CLV2",
+                "CRE1",
+                "CRE2",
+                "CUR1",
+                "CUR2",
+                "DAR1",
+                "DAR2",
+                "ADV1",
+                "ADV2",
+                "AGR1",
+                "AGR2",
+                "COU1",
+                "COU2",
+                "PER1",
+                "PER2",
+                "ROM1",
+                "ROM2",
+                "SOC1",
+                "SOC2",
+                "EMO1",
+                "EMO2",
+                "OUT1",
+                "OUT2",
+                "COM1",
+                "COM2",
+                "PLA1",
+                "PLA2"
+            ]
+        },
+        "Education": {
+            "name": "Education",
+            "values": [
+                "GRADESCHOOL",
+                "HIGHSCHOOL",
+                "TECHSCHOOL",
+                "HIGHGRAD",
+                "COLLEGEGRAD",
+                "COLLEGE",
+                "GRADSCHOOL"
+            ]
+        },
+        "Relationship": {
+            "name": "Relationship",
+            "values": [
+                "MOM",
+                "DAD",
+                "WIFE",
+                "HUSBAND",
+                "SON",
+                "DAUGHTER",
+                "GRANDDAUGHTER",
+                "GRANDSON",
+                "GRANDCHILD",
+                "CHILD",
+                "PARENT",
+                "SPOUSE",
+                "SISTER",
+                "BROTHER",
+                "SIBLING"
+            ]
+        },
+        "Locations": {
+            "name": "Locations",
+            "values": [
+                "UNITY",
+                "BROOKDALE",
+                "MAPLE"
+            ]
+        }
+    },
     "nonModels": {},
-    "version": "96a102c642a91cb14037318a3c916661"
+    "version": "04577ca3e6bc12e52c1e5692e1d3c796"
 };
