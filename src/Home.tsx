090d45a81daa33e87652edcff68266b90a1050f0
kgo6331 <43654121+kgo6331@users.noreply.github.com>
2022-10-18 15:13:32 -0400

    Cleaned up login and home page

--- src/Home.tsx
+++ src/Home.tsx
@@ -7,15 +7,29 @@ import {
     Card,
     useAuthenticator
 } from "@aws-amplify/ui-react";
-
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 function Home() {
     const { signOut } = useAuthenticator()
     return (
         <View className="App">
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="#home">Forget-me-not</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="https://www.leisurecare.com/our-communities/village-unity/">To Unity</Nav.Link>
+                            <Button onClick={() => signOut()}>Sign Out</Button>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
             <Card>
                 <Heading level={1}>We now have Auth!</Heading>
             </Card>
-            <Button onClick={() => signOut()}>Sign Out</Button>
         </View>
     );
 }
