6ac65559600dfc72679e8fa082423d2dd4ff5c87
Chase Chura <clc3020@rit.edu>
2022-11-08 00:44:34 -0500

    Added feed to start button, need to add exit and fullscreen buttons.

--- src/MediaFeed/PatientInfo/PatientProfile.tsx
+++ src/MediaFeed/PatientInfo/PatientProfile.tsx
@@ -11,42 +11,61 @@ import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Button from '@mui/material/Button';
 
-class PatientProfile extends React.Component <{}, {isTablet: boolean}>{
+class PatientProfile extends React.Component <{}, {showMediaFeed: boolean}>{
+    constructor(props: {} | Readonly<{}>) {
+        super(props);
+
+        this.state = {
+            showMediaFeed: false
+        };
+
+        this.toggleShow = this.toggleShow.bind(this);
+    }
+
+    toggleShow() {
+        this.setState({ showMediaFeed: !this.state.showMediaFeed });
+    }
 
     render(){
-        return (
-            <div id="patientProfile">
-                <div className="infoContainer">
-                    <div id="leftContainer">
-                        <Avatar sx={{bgcolor: red[500]}}>
-                            R
-                        </Avatar>
-                        <div id="fullName">
-                            <span>First Name: Lorem</span>
-                            <span>Last Name: Epsim</span>
+        if (this.state.showMediaFeed){
+            return(
+                <MediaFeed></MediaFeed>
+            );
+        }
+        else {
+            return (
+                <div id="patientProfile">
+                    <div className="infoContainer">
+                        <div id="leftContainer">
+                            <Avatar sx={{bgcolor: red[500]}}>
+                                R
+                            </Avatar>
+                            <div id="fullName">
+                                <span>First Name: Lorem</span>
+                                <span>Last Name: Epsim</span>
+                            </div>
+                        </div>
+                        <div id="rightContainer">
+                            <Button variant="contained" onClick={this.toggleShow}>Start Media Feed</Button>
                         </div>
                     </div>
-                    <div id="rightContainer">
-                        <Button variant="contained">Start Media Feed</Button>
+                    <div id="informationDropdown">
+                        <Accordion sx={{bgcolor: '#EFF1FB'}}>
+                            <AccordionSummary
+                                expandIcon={<ExpandMoreIcon/>}
+                                aria-controls="panel1a-content"
+                                id="panel1a-header"
+                            >
+                                <Typography>Detailed patient information</Typography>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                <VerticalLinearStepper></VerticalLinearStepper>
+                            </AccordionDetails>
+                        </Accordion>
                     </div>
                 </div>
-                <div id="informationDropdown">
-                    <Accordion sx={{bgcolor: '#EFF1FB'}}>
-                        <AccordionSummary
-                            expandIcon={<ExpandMoreIcon />}
-                            aria-controls="panel1a-content"
-                            id="panel1a-header"
-                        >
-                            <Typography>Detailed patient information</Typography>
-                        </AccordionSummary>
-                        <AccordionDetails>
-                            <VerticalLinearStepper></VerticalLinearStepper>
-                        </AccordionDetails>
-                    </Accordion>
-                    <MediaFeed></MediaFeed>
-                </div>
-            </div>
-        );
+            );
+        }
     }
 }
 
