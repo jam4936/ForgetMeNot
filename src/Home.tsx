8f11181f6824451b72b764f50d892e6182873271
Jarred Moyer <jarredds941@gmail.com>
2022-11-17 11:48:28 -0500

    Please compile

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,29 +1,12 @@
+
 function Home() {
-    /*
+
     return (
-        <>
-            <Navbar bg="light" expand="lg">
-                <Container>
-                    <Navbar.Brand href="#home">Forget-me-not</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="https://www.leisurecare.com/our-communities/village-unity/">To Unity</Nav.Link>
-                            <button>Sign Out</button>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-            <Card>
-                <Heading level={1}>We now have Auth!</Heading>
-            </Card>
-        </>
-        
-    );
-    */
-   return(<p>test</p>)
-   
+        <h1>
+            This is the home page!
+        </h1>
+);
+
 }
 
 export default Home;
