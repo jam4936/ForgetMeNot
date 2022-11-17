13adc6c75d23cd01fbbf20663743a64b4a00d58d
Jarred Moyer <jarredds941@gmail.com>
2022-11-17 11:39:13 -0500

    changes to make compile work

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,19 +1,7 @@
-import "@aws-amplify/ui-react/styles.css";
-import {
-    withAuthenticator,
-    Button,
-    Heading,
-    View,
-    Card,
-    useAuthenticator
-} from "@aws-amplify/ui-react";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 function Home() {
-    const { signOut } = useAuthenticator()
+    /*
     return (
-        <View className="App">
+        <>
             <Navbar bg="light" expand="lg">
                 <Container>
                     <Navbar.Brand href="#home">Forget-me-not</Navbar.Brand>
@@ -22,7 +10,7 @@ function Home() {
                         <Nav className="me-auto">
                             <Nav.Link href="#home">Home</Nav.Link>
                             <Nav.Link href="https://www.leisurecare.com/our-communities/village-unity/">To Unity</Nav.Link>
-                            <Button onClick={() => signOut()}>Sign Out</Button>
+                            <button>Sign Out</button>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
@@ -30,8 +18,12 @@ function Home() {
             <Card>
                 <Heading level={1}>We now have Auth!</Heading>
             </Card>
-        </View>
+        </>
+        
     );
+    */
+   return(<p>test</p>)
+   
 }
 
-export default withAuthenticator(Home);
+export default Home;
