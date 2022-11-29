f7948792f68c8248a84da772d08a761421b58f63
Kyle McCoy <krm7269@rit.edu>
2022-11-29 18:24:55 -0500

    it works mikayla fixed it

--- frontend/src/Components/AboutYou/AboutYou.tsx
+++ frontend/src/Components/AboutYou/AboutYou.tsx
@@ -2,17 +2,27 @@
 import { useState } from "react";
 import './AboutYou.css';
 import Question from "../../Models/Question";
+import Response from "../../Models/Response"
 import { MenuItem, Select, TextField, Box } from "@mui/material";
 import PersonalityTraits from "./PersonalityTraits/PersonalityTraits";
 import SendResponse from "../../Models/SendResponse";
 import UploadResponseService from "../../Services/UploadResponseService";
 import GetQuestions from "../../Services/GetQuestions";
+import GetResponses from "../../Services/GetResponses"
 export const AboutYou = () => {
     const [questions, setQuestions] = useState<Question[]>();
+    const [responses, setResponses] = useState<Response[]>();
     
 
     var personalityTraits : Question[] = [];
-    
+    const initializeResponses = async () => {
+        await GetResponses.initializeResponses("10");
+        setResponses(GetResponses.responses.sort((a,b) => a.id < b.id ? -1 : a.id > b.id ? 1 : 0));
+
+    }
+    //initializes the response
+    initializeResponses();
+
     const initializeQuestions = async () => {
          await GetQuestions.initializeQuestions("AboutYou");
          setQuestions(GetQuestions.questions.sort((a,b) => a.id < b.id ? -1 : a.id > b.id ? 1 : 0));
@@ -20,6 +30,8 @@ export const AboutYou = () => {
     }
     //initializes the questions
     initializeQuestions();
+
+
 
     const onBlurEvent = (value: string, question : Number) => {
         console.log(value)
@@ -30,13 +42,25 @@ export const AboutYou = () => {
         UploadResponseService.setFormDirty(change, value)
         
     }
+
+    const findResponse = (question: Question) : string =>{
+        let response = responses?.find((x) => x.questionID === question.id)?.response;
+        if(response != undefined){
+            return response;
+        }
+        else if(question.questionType === "select")
+        {
+            return "none"
+        }
+        return "";
+    }
     const getSelect = (question: Question) =>{
         return(
             <div id={question.id.toString()} className={question.questionType}>
                 <label>
                     {question.prompt}
                 </label>
-                <Select id={question.id.toString()} className={question.questionType} defaultValue={"none"} onChange={(event) => onBlurEvent(event.target.value, question.id)}>
+                <Select id={question.id.toString()} className={question.questionType} defaultValue={ findResponse(question) } onChange={(event) => onBlurEvent(event.target.value, question.id)}>
                     <MenuItem value="none" disabled hidden>Select an Option</MenuItem>
                     {question.selectOptions?.map(element => { return <MenuItem value={element}>{element}</MenuItem> })}
                 </Select>
@@ -49,7 +73,7 @@ export const AboutYou = () => {
                 <label htmlFor={question.id.toString()}>
                     {question.prompt}
                 </label>
-                <TextField id={question.id.toString() + "_resp"}className={question.questionType}  onBlur={(event) => onBlurEvent(event.target.value, question.id)} variant="outlined"/>
+                <TextField id={question.id.toString() + "_resp"} defaultValue={ findResponse(question) } className={question.questionType}  onBlur={(event) => onBlurEvent(event.target.value, question.id)} variant="outlined"/>
             </div>
         )
     }
@@ -59,7 +83,7 @@ export const AboutYou = () => {
                 <label>
                     {question.prompt}
                 </label>
-                <TextField id={question.id.toString() + "_resp"}className={question.questionType}  variant="outlined" onBlur={(event) => onBlurEvent(event.target.value, question.id)} rows={4} multiline/>
+                <TextField id={question.id.toString() + "_resp"} defaultValue={ findResponse(question) } className={question.questionType}  variant="outlined" onBlur={(event) => onBlurEvent(event.target.value, question.id)} rows={4} multiline/>
             </div>
         )
     }
