069b56140aab13b155d55c16f8eecc9c661f565b
kgo6331 <43654121+kgo6331@users.noreply.github.com>
2023-03-30 17:48:40 -0400

    Login functionality is complete

--- frontend/src/Components/Authentication/Login.tsx
+++ frontend/src/Components/Authentication/Login.tsx
@@ -1,41 +1,50 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import "./Authentication.css"
 import Email from "./Email/Email";
 import Password from "./Password/Password";
 import LineSeparator from "./LineSeparator/LineSeparator";
 import GoogleSignIn from "./GoogleSignIn/GoogleSignIn"
+import {login} from "../../Services/Authentication";
 
+function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-class Login extends React.Component{
-    constructor(props: {} | Readonly<{}>) {
-        super(props);
+    function handleLogin() {
+        console.log('Hellloooooo')
+        login(email, password)
     }
 
-    render() {
-        return (
-            <div className="AuthFormContainer">
-                <form className="AuthForm">
-                    <div className="AuthFormContent">
-                        <h3 className="AuthFormTitle">
-                            Sign In
-                        </h3>
-                        <Email></Email>
-                        <Password></Password>
-                        <button type="submit" className="btn btn-primary btn-block">
-                            Submit
-                        </button>
-                        <p className="forgot-password text-right">
-                            Forgot <a href="#">password?</a>
-                        </p>
-                        <LineSeparator text="Or Continue With"></LineSeparator>
-                        <GoogleSignIn text="oogle"></GoogleSignIn>
-                        <p id="createAccount">Not registered yet? </p>
-                        <a id="createAccount" href="createAccount"> Create Account</a>
+    return (
+        <div className="AuthFormContainer">
+            <div className="AuthForm">
+                <div className="AuthFormContent">
+                    <h3 className="AuthFormTitle">
+                        Sign In
+                    </h3>
+                    <div className="form-group">
+                        <label>Email Address</label>
+                        <input type="email" onChange={(evt) => {setEmail(evt.target.value)}} className="form-control" placeholder="Email" value={email}/>
                     </div>
-                </form>
+                    <div className="form-group">
+                        <label>Password</label>
+                        <input type="password" className="form-control" placeholder="Password"  onChange={(evt) => {setPassword(evt.target.value)}} value={password}/>
+                    </div>
+                    <button onClick={handleLogin} className="btn btn-primary btn-block">
+                        Login
+                    </button>
+                    <p className="forgot-password text-right">
+                        Forgot <a href="#">password?</a>
+                    </p>
+                    <p id="createAccount">Not registered yet? </p>
+                    <a id="createAccount" href="createAccount"> Create Account</a>
+                </div>
             </div>
-        );
-    }
+        </div>
+    )
 }
+
+
+
 
 export default Login;
