a6271a34f4529d94613c6906c3ee9210c4f8a07f
Kyle McCoy <krm7269@rit.edu>
2023-04-04 17:54:25 -0400

    login to the right

--- frontend/src/NavigationBar.tsx
+++ frontend/src/NavigationBar.tsx
@@ -18,43 +18,47 @@ export default function NavigationBar() {
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
-                {!isAdmin() ? <div></div> :
-                    <Nav className="mr-auto">
-                        <Nav.Link href="questionControl"> Question Control </Nav.Link>
-                        <NavDropdown title="Senior Project" id="collapsible-nav-dropdown">
-                            <NavDropdown.Item href="project"> Project </NavDropdown.Item>
-                            <NavDropdown.Item href="visionConcept"> Vision Concept </NavDropdown.Item>
-                            <NavDropdown.Item href="dev"> Dev </NavDropdown.Item>
-                        </NavDropdown>
-                        <NavDropdown title="Family" id="collapsible-nav-dropdown">
-                            <NavDropdown.Item href="familyForm"> Family Form </NavDropdown.Item>
-                            <NavDropdown.Item href="mediaFeed"> Media Feed </NavDropdown.Item>
-                            <NavDropdown.Item href="patientCalendar"> Patient Calendar </NavDropdown.Item>
-                        </NavDropdown>
-                        <NavDropdown title="Facility" id="collapsible-nav-dropdown">
-                            <NavDropdown.Item href="patientInfo"> Patient Info </NavDropdown.Item>
-                            <NavDropdown.Item href="menu"> Menu </NavDropdown.Item>
-                            <NavDropdown.Item href="facultyCalendar"> Facility Calendar </NavDropdown.Item>
-                        </NavDropdown>
+                <div>
+                    {!isAdmin() ? <div></div> :
+                        <Nav className="mr-auto">
+                            <Nav.Link href="questionControl"> Question Control </Nav.Link>
+                            <NavDropdown title="Senior Project" id="collapsible-nav-dropdown">
+                                <NavDropdown.Item href="project"> Project </NavDropdown.Item>
+                                <NavDropdown.Item href="visionConcept"> Vision Concept </NavDropdown.Item>
+                                <NavDropdown.Item href="dev"> Dev </NavDropdown.Item>
+                            </NavDropdown>
+                            <NavDropdown title="Family" id="collapsible-nav-dropdown">
+                                <NavDropdown.Item href="familyForm"> Family Form </NavDropdown.Item>
+                                <NavDropdown.Item href="mediaFeed"> Media Feed </NavDropdown.Item>
+                                <NavDropdown.Item href="patientCalendar"> Patient Calendar </NavDropdown.Item>
+                            </NavDropdown>
+                            <NavDropdown title="Facility" id="collapsible-nav-dropdown">
+                                <NavDropdown.Item href="patientInfo"> Patient Info </NavDropdown.Item>
+                                <NavDropdown.Item href="menu"> Menu </NavDropdown.Item>
+                                <NavDropdown.Item href="facultyCalendar"> Facility Calendar </NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    }
+                    {!isFamily() ? <div></div> :
+                        <Nav className="mr-auto">
+                            <Nav.Link href="familyForm"> Family Form </Nav.Link>
+                            <Nav.Link href="mediaFeed"> Media Feed </Nav.Link>
+                            <Nav.Link href="patientCalendar"> Patient Calendar </Nav.Link>
+                        </Nav>
+                    }
+                    {!isFacility() ? <div></div> :
+                        <Nav className={"mr-auto"}>
+                            <Nav.Link href="patientInfo"> Patient Information </Nav.Link>
+                            <Nav.Link href="menu"> Menu </Nav.Link>
+                            <Nav.Link href="facultyCalendar"> Facility Calendar </Nav.Link>
+                        </Nav>
+                    }
+                </div>
+                <div>
+                    <Nav className={"login"}>
+                        {isAuthenticated() ? (<Nav.Link href="/" onClick={logout}>Logout</Nav.Link>):(<Nav.Link className={"login"} href="login">Login</Nav.Link>)}
                     </Nav>
-                }
-                {!isFamily() ? <div></div> :
-                    <Nav className="mr-auto">
-                        <Nav.Link href="familyForm"> Family Form </Nav.Link>
-                        <Nav.Link href="mediaFeed"> Media Feed </Nav.Link>
-                        <Nav.Link href="patientCalendar"> Patient Calendar </Nav.Link>
-                    </Nav>
-                }
-                {!isFacility() ? <div></div> :
-                    <Nav className={"mr-auto"}>
-                        <Nav.Link href="patientInfo"> Patient Information </Nav.Link>
-                        <Nav.Link href="menu"> Menu </Nav.Link>
-                        <Nav.Link href="facultyCalendar"> Facility Calendar </Nav.Link>
-                    </Nav>
-                }
-                <Nav>
-                    {isAuthenticated() ? (<Nav.Link href="/" onClick={logout}>Logout</Nav.Link>):(<Nav.Link href="login">Login</Nav.Link>)}
-                </Nav>
+                </div>
             </Navbar.Collapse>
         </Navbar>
     )
